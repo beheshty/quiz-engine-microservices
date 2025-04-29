582684d9f494a5d53f9d5457c4e644ad9dbef500
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-04-29 11:36:26 +0330

    feat: add basid repository interfaces

---
+++ src/Shared/Shared.Domain/Repositories/IReadOnlyRepository.cs
@@ -0,0 +1,19 @@
+﻿using QuizEngineMicroservices.Shared.Domain;
+using System.Linq.Expressions;
+
+namespace Shared.Domain.Repositories
+{
+    public interface IReadOnlyRepository<TEntity> where TEntity : class, IEntity
+    {
+        Task<bool> AnyAsync(CancellationToken cancellationToken = default);
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+        Task<List<TEntity>> GetListAsync(CancellationToken cancellationToken = default);
+        Task<long> GetCountAsync(CancellationToken cancellationToken = default);
+    }
+
+    public interface IReadOnlyRepository<TEntity, TKey> : IReadOnlyRepository<TEntity> where TEntity : class, IEntity<TKey>
+    {
+        Task<TEntity> GetAsync(TKey id, CancellationToken cancellationToken = default);
+        Task<TEntity?> FindAsync(TKey id, CancellationToken cancellationToken = default);
+    }
+}
