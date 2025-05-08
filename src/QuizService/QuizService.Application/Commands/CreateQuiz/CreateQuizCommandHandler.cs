edda2195c0a2719704ac4dbe3e95d8daabdc934b
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-05-08 10:08:55 +0330

    feat: add new validation to quiz create command for checking question existance

--- src/QuizService/QuizService.Application/Commands/CreateQuiz/CreateQuizCommandHandler.cs
+++ src/QuizService/QuizService.Application/Commands/CreateQuiz/CreateQuizCommandHandler.cs
@@ -1,20 +1,33 @@
 using QuizService.Application.Common.CQRS.Interfaces;
+using QuizService.Application.Services;
 using QuizService.Domain.Entities.QuizManagement;
 using QuizService.Domain.Repositories;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace QuizService.Application.Commands.CreateQuiz;
 
 public class CreateQuizCommandHandler : ICommandHandler<CreateQuizCommand, Guid>
 {
     private readonly IQuizRepository _quizRepository;
+    private readonly IQuestionValidationService _questionValidationService;
 
-    public CreateQuizCommandHandler(IQuizRepository quizRepository)
+    public CreateQuizCommandHandler(
+        IQuizRepository quizRepository,
+        IQuestionValidationService questionValidationService)
     {
         _quizRepository = quizRepository;
+        _questionValidationService = questionValidationService;
     }
 
     public async Task<Guid> Handle(CreateQuizCommand command, CancellationToken cancellationToken = default)
     {
+        // Validate that all questions exist
+        var questionIds = command.Quiz.Questions.Select(q => q.QuestionId);
+        await _questionValidationService.ValidateQuestionsExistAsync(questionIds, cancellationToken);
+
         var quizId = Guid.NewGuid();
         var quiz = new Quiz(quizId, command.Quiz.Title, command.Quiz.Description);
 
