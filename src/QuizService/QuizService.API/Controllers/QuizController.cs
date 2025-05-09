e4f1bd5934e9a9885d7277412ac064de6537b77e
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-05-09 17:54:23 +0330

    Add ChangeQuizStatus endpoint to QuizController
    This commit introduces a new HTTP PATCH endpoint for changing the status of an existing quiz in `QuizController.cs`.

--- src/QuizService/QuizService.API/Controllers/QuizController.cs
+++ src/QuizService/QuizService.API/Controllers/QuizController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using QuizService.Application.Commands.CreateQuiz;
 using QuizService.Application.Commands.UpdateQuiz;
+using QuizService.Application.Commands.ChangeQuizStatus;
 using QuizService.Application.Common.CQRS.Interfaces;
+using QuizService.Domain.Enums;
 
 namespace QuizService.API.Controllers;
 
@@ -44,18 +46,39 @@ public class QuizController : ControllerBase
     /// <returns>The ID of the updated quiz</returns>
     /// <response code="200">Returns the updated quiz ID</response>
     /// <response code="400">If the quiz data is invalid</response>
-    /// <response code="404">If the quiz is not found</response>
-    /// <response code="409">If the quiz is not in Draft status</response>
     [HttpPut("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status409Conflict)]
     public async Task<ActionResult<Guid>> UpdateQuiz(Guid id, UpdateQuizDto quiz, CancellationToken cancellationToken = default)
     {
         var command = new UpdateQuizCommand(id, quiz);
         var quizId = await _dispatcher.Send(command, cancellationToken);
         
         return Ok(quizId);
+    }
+
+    /// <summary>
+    /// Changes the status of an existing quiz
+    /// </summary>
+    /// <param name="id">The ID of the quiz to update</param>
+    /// <param name="newStatus">The new status to set</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The new status of the quiz</returns>
+    /// <response code="200">Returns the new quiz status</response>
+    /// <response code="400">If the request is invalid</response>
+    [HttpPatch("{id}/status")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<QuizStatus>> ChangeQuizStatus(Guid id, [FromBody] QuizStatus newStatus, CancellationToken cancellationToken = default)
+    {
+        var command = new ChangeQuizStatusCommand
+        {
+            QuizId = id,
+            NewStatus = newStatus
+        };
+        
+        var status = await _dispatcher.Send(command, cancellationToken);
+        return Ok(status);
     }
 } 
