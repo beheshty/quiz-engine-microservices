3a5e6a13d824d9715f68e64a86e433c7dbb10426
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-05-09 17:15:24 +0330

    feat: Add validation attributes to UpdateQuizDto and QuizQuestionDto

--- src/QuizService/QuizService.Application/Commands/UpdateQuiz/UpdateQuizDto.cs
+++ src/QuizService/QuizService.Application/Commands/UpdateQuiz/UpdateQuizDto.cs
@@ -1,15 +1,27 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace QuizService.Application.Commands.UpdateQuiz;
 
 public class UpdateQuizDto
 {
+    [Required]
+    [StringLength(200)]
     public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(1000)]
     public string Description { get; set; } = string.Empty;
+
+    [Required]
     public List<QuizQuestionDto> Questions { get; set; } = [];
 }
 
 public class QuizQuestionDto
 {
+    [Required]
     public Guid QuestionId { get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue)]
     public int Order { get; set; }
 }
