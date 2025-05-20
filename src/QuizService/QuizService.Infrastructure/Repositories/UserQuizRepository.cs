ed05b26634937f369ffff0c8d43979a2b1052373
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-05-20 16:13:08 +0330

    feat: Implement GetAsync method in UserQuizRepository with entity not found exception handling
    Enhances the `UserQuizRepository` by overriding the `GetAsync` method to include eager loading of answers and throw an `EntityNotFoundException` if the specified `UserQuiz` entity is not found. This improves error handling and ensures that the repository adheres to best practices for data retrieval.

--- src/QuizService/QuizService.Infrastructure/Repositories/UserQuizRepository.cs
+++ src/QuizService/QuizService.Infrastructure/Repositories/UserQuizRepository.cs
@@ -1,7 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 using QuizService.Domain.Entities.QuizRuntime;
 using QuizService.Domain.Repositories;
 using QuizService.Infrastructure.Data;
 using QuizService.Infrastructure.Repositories.Base;
+using Shared.Domain.Exceptions;
 
 namespace QuizService.Infrastructure.Repositories;
 
@@ -9,5 +11,15 @@ public class UserQuizRepository : EfRepository<UserQuiz, Guid>, IUserQuizReposit
 {
     public UserQuizRepository(QuizDbContext context) : base(context)
     {
+    }
+
+    public async override Task<UserQuiz> GetAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        var entity = await DbSet.Include(u => u.Answers).FirstOrDefaultAsync(u => u.Id == id);
+        if (entity == null)
+        {
+            throw new EntityNotFoundException(typeof(UserQuiz), id);
+        }
+        return entity;
     }
 } 
