520c568c054c2716e52e0eda62a909f887938f38
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-05-23 15:38:21 +0330

    feat: Implement LocalEventBus for synchronous and asynchronous event publishing
    This commit introduces the `LocalEventBus` class, implementing the `ILocalEventBus` interface. It provides methods for publishing events both synchronously and asynchronously, leveraging dependency injection to retrieve event handlers. This addition enhances the event-driven architecture by facilitating local event handling within the microservices framework.

---
+++ src/BuildingBlocks/BuildingBlocks.EventBus/BuildingBlocks.EventBus/Local/LocalEventBus.cs
@@ -0,0 +1,25 @@
+using BuildingBlocks.EventBus.Abstraction.Local;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.EventBus.Local;
+
+public class LocalEventBus(IServiceProvider serviceProvider) : ILocalEventBus
+{
+    public void Publish<TEvent>(TEvent eventData) where TEvent : class
+    {
+        var handlers = serviceProvider.GetServices<ILocalEventHandler<TEvent>>();
+        foreach (var handler in handlers)
+        {
+            handler.Handle(eventData);
+        }
+    }
+
+    public async Task PublishAsync<TEvent>(TEvent eventData, CancellationToken cancellationToken = default) where TEvent : class
+    {
+        var handlers = serviceProvider.GetServices<ILocalEventHandler<TEvent>>();
+        foreach (var handler in handlers)
+        {
+            await handler.HandleAsync(eventData, cancellationToken);
+        }
+    }
+}
