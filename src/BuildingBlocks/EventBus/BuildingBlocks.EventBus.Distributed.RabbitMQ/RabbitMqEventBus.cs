82a65027288b34e71c8b7b81d87c9511e17ebe1c
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-05-28 13:42:53 +0330

    feat: Implement RabbitMqEventBus for distributed event publishing
    This commit introduces the `RabbitMqEventBus` class, which implements the `IDistributedEventBus` interface using MassTransit for publishing events to RabbitMQ. This addition enhances the event bus functionality by providing a concrete implementation for asynchronous event publishing.

---
+++ src/BuildingBlocks/EventBus/BuildingBlocks.EventBus.Distributed.RabbitMQ/RabbitMqEventBus.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.EventBus.Abstraction.Distributed;
+using MassTransit;
+
+namespace BuildingBlocks.EventBus.Distributed.RabbitMQ
+{
+    internal class RabbitMqEventBus : IDistributedEventBus
+    {
+        private readonly IBus _bus;
+
+        internal RabbitMqEventBus(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public async Task PublishAsync<TEvent>(TEvent eventData, CancellationToken cancellationToken = default) where TEvent : class
+        {
+            await _bus.Publish(eventData, cancellationToken);
+        }
+    }
+}
