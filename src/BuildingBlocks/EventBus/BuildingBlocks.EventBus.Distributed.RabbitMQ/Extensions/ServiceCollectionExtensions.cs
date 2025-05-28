bcc675bbbcf951c673feee49c80911de8853a56b
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-05-28 13:43:34 +0330

    feat: Add ServiceCollectionExtensions for RabbitMQ event bus configuration
    This commit introduces the `ServiceCollectionExtensions` class, which provides an extension method `AddRabbitMqMassTransitEventBus` for configuring RabbitMQ with MassTransit. This method allows for the registration of event consumers and the setup of message handling based on attributes, enhancing the integration of RabbitMQ within the event bus architecture.

---
+++ src/BuildingBlocks/EventBus/BuildingBlocks.EventBus.Distributed.RabbitMQ/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,77 @@
+﻿using BuildingBlocks.EventBus.Abstraction.Distributed;
+using BuildingBlocks.EventBus.Distributed.RabbitMQ.Attributes;
+using BuildingBlocks.EventBus.Distributed.RabbitMQ.Configuration;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.EventBus.Distributed.RabbitMQ.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+
+        public static IServiceCollection AddRabbitMqMassTransitEventBus(
+                        this IServiceCollection services,
+                        Assembly assembly,
+                        Action<RabbitMqOptions> configureOptions)
+        {
+
+            var options = new RabbitMqOptions();
+            configureOptions?.Invoke(options);
+
+            services.AddScoped(typeof(RabbitMqEventConsumer<>));
+            services.AddScoped<IDistributedEventBus, RabbitMqEventBus>();
+
+            services.AddMassTransit(cfg =>
+            {
+                cfg.AddConsumers(assembly);
+
+                cfg.UsingRabbitMq((context, busCfg) =>
+                {
+                    busCfg.Host(new Uri(options.HostUrl), h =>
+                    {
+                        if (!string.IsNullOrWhiteSpace(options.Username) && !string.IsNullOrWhiteSpace(options.Password))
+                        {
+                            h.Username(options.Username);
+                            h.Password(options.Password);
+                        }
+                    });
+
+                    busCfg.ConfigureWithAttributes(cfg, context, assembly);
+                });
+            });
+
+            return services;
+        }
+
+        private static void ConfigureWithAttributes(this IRabbitMqBusFactoryConfigurator rfc, IBusRegistrationConfigurator brc,
+                                                   IRegistrationContext context, Assembly assembly)
+        {
+            var messageTypesWithAttributes = assembly
+            .GetTypes()
+            .Where(t => t.GetCustomAttribute<DistributedMessageAttribute>() != null);
+
+            foreach (var messageType in messageTypesWithAttributes)
+            {
+                var attribute = messageType.GetCustomAttribute<DistributedMessageAttribute>();
+
+                var consumerType = typeof(RabbitMqEventConsumer<>).MakeGenericType(messageType);
+                if (consumerType == null)
+                    continue;
+                brc.AddConsumer(consumerType);
+
+                rfc.ReceiveEndpoint(attribute.QueueName, e =>
+                {
+                    e.Bind(attribute.ExchangeName, x =>
+                    {
+                        x.ExchangeType = attribute.ExchangeType.ToString().ToLower();
+                        x.RoutingKey = messageType.FullName;
+                        x.Durable = attribute.Durable;
+                    });
+
+                    e.ConfigureConsumer(context, consumerType);
+                });
+            }
+        }
+    }
+}
