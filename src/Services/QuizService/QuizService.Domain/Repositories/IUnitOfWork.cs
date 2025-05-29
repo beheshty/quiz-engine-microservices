d5a9e579acb646aeba7a588c30cc70f35e5bf1d9
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-05-29 12:19:44 +0330

    feat: Introduce IUnitOfWork interface and register it in ServiceCollectionExtensions
    This commit adds the `IUnitOfWork` interface to facilitate unit of work pattern implementation, allowing for asynchronous saving of changes. Additionally, it registers the `IUnitOfWork` in the `ServiceCollectionExtensions` for dependency injection, enhancing the overall architecture of the QuizService.

---
+++ src/Services/QuizService/QuizService.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+
+namespace QuizService.Domain.Repositories
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+} 
