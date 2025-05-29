67c668bbd49457c7451956b7aab7ada2d88f05fd
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-05-29 18:15:41 +0330

    feat: Introduce DistributedMessageAttribute for event bus messaging
    This commit adds a new `DistributedMessageAttribute` class to the `BuildingBlocks.EventBus.Abstraction` namespace, replacing the previous implementation in `BuildingBlocks.EventBus.Distributed.RabbitMQ`. The new attribute includes properties for `Destination`, `DistributionStrategy`, and `Subscription`, enhancing the flexibility and clarity of event bus messaging configurations.

---
+++ src/BuildingBlocks/EventBus/BuildingBlocks.EventBus.Abstraction/Distributed/DistributedMessageAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace BuildingBlocks.EventBus.Distributed.RabbitMQ.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class DistributedMessageAttribute : Attribute
+    {
+        public string Destination { get; set; } = "event_bus";
+        public string DistributionStrategy { get; set; } = "fanout";
+        public string Subscription { get; set; }
+
+        public DistributedMessageAttribute(string subscription)
+        {
+            Subscription = subscription;
+        }
+    }
+}
