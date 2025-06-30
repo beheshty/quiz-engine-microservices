b9a7e1d0baa7bc01e583e83ea409b35ae88c2854
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-06-30 11:06:16 +0330

    feat: add user grpc client interface and implementation
    Add IUserGrpcClient interface and UserGrpcClient implementation to fetch user information via gRPC. This enables the notification service to retrieve user details needed for notifications.

---
+++ src/Services/NotificationService/NotificationService.Application/Grpc/UserGrpcClient.cs
@@ -0,0 +1,25 @@
+﻿
+using Shared.User.Grpc.Proto;
+
+namespace NotificationService.Application.Grpc
+{
+    public class UserGrpcClient : IUserGrpcClient
+    {
+        private readonly Users.UsersClient _usersClient;
+
+        public UserGrpcClient(Users.UsersClient usersClient)
+        {
+            _usersClient = usersClient;
+        }
+
+        public async Task<UserInfo> GetUserInfoAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            var userRequest = new GetUserInfoRequest()
+            {
+                UserId = userId.ToString(),
+            };
+            var result =  await _usersClient.GetUserInfoByIdAsync(userRequest, cancellationToken: cancellationToken);
+            return result.User;
+        }
+    }
+}
